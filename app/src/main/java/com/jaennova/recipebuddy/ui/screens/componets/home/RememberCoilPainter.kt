6ec007250a074f481b490724e75abddfb63c207c
Jaen <jaennova9@gmail.com>
2024-10-27 00:25:01 -0600

    Create RememberCoilPainter.kt

---
+++ app/src/main/java/com/jaennova/recipebuddy/ui/screens/componets/home/RememberCoilPainter.kt
@@ -0,0 +1,20 @@
+package com.jaennova.recipebuddy.ui.screens.componets.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import com.jaennova.recipebuddy.R
+
+@Composable
+fun rememberCoilPainter(request: String): Painter {
+    return rememberAsyncImagePainter(
+        ImageRequest.Builder(LocalContext.current).data(data = request)
+            .apply(block = fun ImageRequest.Builder.() {
+                crossfade(true)
+                placeholder(R.drawable.ic_launcher_foreground)
+                error(R.drawable.ic_launcher_foreground)
+            }).build()
+    )
+}
