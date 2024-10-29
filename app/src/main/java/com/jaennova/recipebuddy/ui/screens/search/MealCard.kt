c2b31547390a8dfb139a97dba2c0be1caf7c01a5
Jaen <jaennova9@gmail.com>
2024-10-28 21:25:59 -0600

    Update MealCard.kt

--- app/src/main/java/com/jaennova/recipebuddy/ui/screens/search/MealCard.kt
+++ app/src/main/java/com/jaennova/recipebuddy/ui/screens/search/MealCard.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,7 +20,6 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.jaennova.recipebuddy.data.model.Meal
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MealCard(
     meal: Meal,
