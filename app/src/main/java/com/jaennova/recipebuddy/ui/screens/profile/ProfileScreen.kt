6309bcd269e5b48e86f92c605bacd73f4d62f677
Jaen <jaennova9@gmail.com>
2024-10-29 12:14:53 -0600

    Update ProfileScreen.kt

--- app/src/main/java/com/jaennova/recipebuddy/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/jaennova/recipebuddy/ui/screens/profile/ProfileScreen.kt
@@ -18,13 +18,8 @@ fun ProfileScreen(navController: NavController) {
         bottomBar = { RecipeBottomNavigation(navController) }
     ) { paddingValues ->
         Column(modifier = Modifier.padding(paddingValues)) {
-            CardProfile()
-            SavedSection()
-            CardRecipeSaved(
-                foodImage = "https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg",
-                foodName = "Beef and Mustard Pie",
-                onClick = {}
-            )
+                CardProfile()
+                SavedSection()
         }
     }
 }
